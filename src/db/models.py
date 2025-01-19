4c2d029a8e6de38d216a76e1af87790f00338b8d
Saravanan P <saravanancopper@gmail.com>
2025-01-19 23:53:24 +0530

    remove env path in db models initialization file

--- src/db/models.py
+++ src/db/models.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 from pathlib import Path
 
-env_path = Path(__file__).parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 Base = declarative_base()
 
