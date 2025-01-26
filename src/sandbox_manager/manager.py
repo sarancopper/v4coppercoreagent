c2356ec5cf84c408895266a0cdd87c9eae78f508
Saravanan P <saravanancopper@gmail.com>
2025-01-27 04:25:49 +0530

    initial file foe sandbox manager

---
+++ src/sandbox_manager/manager.py
@@ -0,0 +1,6 @@
+import docker
+
+def run_validation_in_docker(code_snippet):
+    # Build an image or volume with the code
+    # Run a container that tries to compile or lint the code
+    return success_or_fail
